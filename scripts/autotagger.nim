e1b2acdfbec8873b2430e7dcc43c9e5957439e8c
Technisha <proxneon960@gmail.com>
2022-08-30 21:59:45 +0100

    Fixed actions again

--- scripts/autotagger.nim
+++ scripts/autotagger.nim
@@ -9,25 +9,29 @@ const REPO = "github.com/Mythical-Forest-Collective/nim-commit-test.git"
 
 var REPO_URL = "https://" & REPO
 
-if getEnv("GITHUB_TOKEN") != "":  # Github Actions-specific, can be modified if other hosts have something similar
+if getEnv("GITHUB_TOKEN") != "": # Github Actions-specific, can be modified if other hosts have something similar
   REPO_URL = "https://" & getEnv("GITHUB_TOKEN") & "@" & REPO
 
-proc semver(versionString:string):seq[int] = split(versionString, '.', 3).map(parseInt)
+proc semver(versionString: string): seq[int] = split(versionString, '.', 3).map(parseInt)
 
 discard execCmd("rm -rf .cache")
 
 
 discard execCmd(fmt"git clone {REPO_URL} .cache/head") # Clone the head
-var prevCommit = execCmdEx("git rev-parse HEAD^", workingDir=".cache/head").output.replace("\n", "") # Get previous commit hash
+var prevCommit = execCmdEx("git rev-parse HEAD^",
+    workingDir = ".cache/head").output.replace("\n", "") # Get previous commit hash
 
 
 discard execCmd(fmt"git clone -n {REPO_URL} .cache/prevCommit") # Clone new repo so we can change to prev commit
 echo fmt"git checkout {prevCommit}"
-discard execCmdEx(fmt"git checkout {prevCommit}", workingDir=".cache/prevCommit")
+discard execCmdEx(fmt"git checkout {prevCommit}",
+    workingDir = ".cache/prevCommit")
 
 
-var currPkgVerStr = execCmdEx("nimble dump --json", workingDir=".cache/head").output.parseJson()["version"].getStr
-var prevPkgVerStr = execCmdEx("nimble dump --json", workingdir=".cache/prevCommit").output.parseJson()["version"].getStr
+var currPkgVerStr = execCmdEx("nimble dump --json",
+    workingDir = ".cache/head").output.parseJson()["version"].getStr
+var prevPkgVerStr = execCmdEx("nimble dump --json",
+    workingdir = ".cache/prevCommit").output.parseJson()["version"].getStr
 
 
 var currPkgVer = currPkgVerStr.semver
@@ -45,11 +49,14 @@ if currPkgVerStr == prevPkgVerStr:
 
 echo "\n\n"
 
-echo execCmdEx(fmt "GIT_COMMITTER_DATE=\"$(git show --format=%aD | head -1)\" git tag -a v{prevPkgVerStr} {prevCommit} -am \"Release v{prevPkgVerStr} as commit hash \\`{prevCommit}\\`\"", workingDir=".cache/head").output
+echo execCmdEx(fmt "GIT_COMMITTER_DATE=\"$(git show --format=%aD | head -1)\" git tag -a v{prevPkgVerStr} {prevCommit} -am \"Release v{prevPkgVerStr} as commit hash \\`{prevCommit}\\`\"",
+    workingDir = ".cache/head").output
 
 echo "\n\n"
 
-echo execCmdEx(fmt"git push origin v{prevPkgVerStr}", workingDir=".cache/head").output
+echo execCmdEx(fmt"git push origin v{prevPkgVerStr}",
+    workingDir = ".cache/head").output
 
+discard execCmd("rm -rf .cache")
 
 quit("Created tag for last version!", 0)
