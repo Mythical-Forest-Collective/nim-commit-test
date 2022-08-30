40ab7dbe3a7a5ec5c8b2d510af61c7b6a9ff6b8c
Technisha <proxneon960@gmail.com>
2022-08-30 21:34:44 +0100

    Sweet

--- scripts/autotagger.nim
+++ scripts/autotagger.nim
@@ -3,19 +3,25 @@ import strutils
 import sequtils
 import json
 import osproc
+import os
 
-const REPO = "https://github.com/Mythical-Forest-Collective/nim-commit-test"
+const REPO = "github.com/Mythical-Forest-Collective/nim-commit-test.git"
+
+var REPO_URL = "https://" & REPO
+
+if getEnv("GITHUB_TOKEN") != "":  # Github Actions-specific, can be modified if other hosts have something similar
+  REPO_URL = "https://" & getEnv("GITHUB_TOKEN") & "@" & REPO
 
 proc semver(versionString:string):seq[int] = split(versionString, '.', 3).map(parseInt)
 
 discard execCmd("rm -rf .cache")
 
 
-discard execCmd(fmt"git clone {REPO} .cache/head") # Clone the head
+discard execCmd(fmt"git clone {REPO_URL} .cache/head") # Clone the head
 var prevCommit = execCmdEx("git rev-parse HEAD^", workingDir=".cache/head").output.replace("\n", "") # Get previous commit hash
 
 
-discard execCmd(fmt"git clone -n {REPO} .cache/prevCommit") # Clone new repo so we can change to prev commit
+discard execCmd(fmt"git clone -n {REPO_URL} .cache/prevCommit") # Clone new repo so we can change to prev commit
 echo fmt"git checkout {prevCommit}"
 discard execCmdEx(fmt"git checkout {prevCommit}", workingDir=".cache/prevCommit")
 
@@ -31,9 +37,9 @@ var prevPkgVer = prevPkgVerStr.semver
 echo currPkgVer, ", ", prevPkgVer
 
 
-if not currPkgVer[0] > prevPkgVer[0]:
-  if not currPkgVer[1] > prevPkgVer[1]:
-    if not currPkgVer[2] > prevPkgVer[2]:
+if currPkgVer[0] <= prevPkgVer[0]:
+  if currPkgVer[1] <= prevPkgVer[1]:
+    if currPkgVer[2] <= prevPkgVer[2]:
       quit("No new version! Not creating a tag for the last version!", 0)
 
 echo "\n\n"
